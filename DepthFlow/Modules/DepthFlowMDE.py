841cf713e70fc9cd71ca5a9d7560ac73552087b2
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-02-25 04:04:01 -0400

    Rename Halo and move expensive imports

--- DepthFlow/Modules/DepthFlowMDE.py
+++ DepthFlow/Modules/DepthFlowMDE.py
@@ -58,7 +58,7 @@ class DepthFlowMDE:
         self.load_model()
 
         # Estimate Depth Map
-        with halo.Halo(text=f"Estimating Depth Map for the input image (CUDA: {self.cuda})"):
+        with Halo(text=f"Estimating Depth Map for the input image (CUDA: {self.cuda})"):
             depth_map = self.model.infer_pil(image)
 
         # -----------------------------------------------------------------------------------------|
--- DepthFlow/__init__.py
+++ DepthFlow/__init__.py
@@ -1,34 +1,6 @@
-import os
-
 import DepthFlow.Resources as DepthFlowResources
-
-# -------------------------------------------------------------------------------------------------|
-# Hack to install path dependencies at runtime.
-while bool(os.environ.get("PYAPP", False)):
-    try:
-        import Broken
-        break
-    except ImportError:
-        print("Installing path dependencies... (Any errors should be ok to ignore)")
-
-    import importlib.resources
-    import subprocess
-    import sys
-
-    # Fixme: Why PYAPP_PASS_LOCATION isn't passed on Linux?
-    if os.name != "nt":
-        sys.argv = sys.argv[1:]
-        sys.argv.insert(0, sys.executable)
-
-    # Pip acronym and install maybe required packages
-    PIP = [sys.executable, "-m", "pip", "install", "--disable-pip-version-check", "--quiet"]
-
-    # Install bundled wheels.. in our wheel
-    for wheel in (importlib.resources.files(DepthFlowResources)/"Wheels").glob("*.whl"):
-        subprocess.run(PIP + [str(wheel)])
-    break
-# -------------------------------------------------------------------------------------------------|
-
+import torch
+import transformers
 from ShaderFlow import *
 
 from Broken import *
