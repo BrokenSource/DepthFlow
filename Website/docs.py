8f83ddf0d1a1ad5b8df90698276ff2ad091b08b4
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-09-28 11:52:02 -0400

    Add initial documentation maker file and parameters docs

--- Website/docs.py
+++ Website/docs.py
@@ -1,13 +1,362 @@
-import sys
+import math
+import os
+from pathlib import Path
+from typing import TYPE_CHECKING, Callable, Iterable, Type
 
-from Broken import shell
+import imgui
+from attr import Factory, define
+from DepthFlow import DepthScene
+from ShaderFlow.Variable import ShaderVariable
 
-try:
+from Broken import BROKEN, OnceTracker, install, log
+from Broken.Externals.Depthmap import DepthAnythingV2, DepthEstimator
+from Broken.Externals.FFmpeg import FFmpegAudioCodecNone
+
+install("manim", "minio")
+
+if TYPE_CHECKING:
     import manim
-except ImportError:
-    shell(sys.executable, "-m", "uv", "pip", "install", "manim")
-    import manim
+    import minio
 
+os.environ["IMGUI_FONT_SCALE"] = "1.21"
+
+def _dir(path: str) -> str:
+    return f"[bold blue]{path}[reset]"
+
+# ------------------------------------------------------------------------------------------------ #
+# Todo: Move Minio and Manim common classes to a DocsFabric module
+
+@define
+class BrokenMinio:
+
+    # Authentication
+    access_key: str = os.getenv("MINIO_ACCESS_KEY")
+    secret_key: str = os.getenv("MINIO_SECRET_KEY")
+    endpoint:   str = os.getenv("MINIO_ENDPOINT")
+    bucket:     str = os.getenv("MINIO_BUCKET")
+
+    authenticated: bool = False
+    client: minio.Minio = None
+
+    def __attrs_post_init__(self):
+        try:
+            self.client = minio.Minio(
+                endpoint=self.endpoint,
+                access_key=self.access_key,
+                secret_key=self.secret_key,
+                secure=True
+            )
+            self.authenticated = True
+            log.success(f"Connected to Minio instance at ({self.endpoint})")
+            self.make_bucket(self.bucket)
+        except Exception:
+            log.warning("Failed to connect to Minio instance at ({self.endpoint})")
+
+    def guarded(method: Callable) -> Callable:
+        def wrapper(self, *args, **kwargs):
+            if self.authenticated:
+                return method(self, *args, **kwargs)
+            log.warning("Minio instance is not authenticated")
+        return wrapper
+
+    @guarded
+    def make_bucket(self, bucket: str):
+        if not self.client.bucket_exists(bucket):
+            self.client.make_bucket(bucket)
+
+    @guarded
+    def upload(self, local: Path, remote: Path) -> None:
+        log.info(f"Uploading Minio file {_dir(local)} → {_dir(f'/{remote}@{self.bucket}')}")
+        self.client.fput_object(self.bucket, str(remote), str(local))
+
+    @guarded
+    def download(self, remote: Path, local: Path) -> Path:
+        log.info(f"Downloading Minio file {_dir(f'/{remote}@{self.bucket}')} → {_dir(local)}")
+        self.client.fget_object(
+            bucket_name=self.bucket,
+            object_name=str(remote),
+            file_path=str(local),
+        )
+        return local
+
+    @guarded
+    def remove(self, remote: Path) -> None:
+        log.info(f"Removing Minio file {_dir(f'/{remote}@{self.bucket}')}")
+        self.client.remove_object(self.bucket, str(remote))
+
+    @guarded
+    def rmdir(self, remote: Path) -> None:
+        log.info(f"Removing Minio directory {_dir(f'/{remote}@{self.bucket}')}")
+
+        for object in self.client.list_objects(self.bucket, prefix=f"{remote}/", recursive=True):
+            log.info(f"• Deleted {_dir(f'/{object.object_name}@{self.bucket}')}")
+            self.client.remove_object(self.bucket, object.object_name)
+
+# ------------------------------------------------------------------------------------------------ #
+
+SHADER_PATCH = """
+if (iStaticPlane && abs(point_height - iDepthStatic) < 0.002) {
+    fragColor = vec4(255.0, 79.0, 0, 255.0)/255.0;
+    return;
+}
+if (iFocusPlane && abs(point_height - iDepthFocus) < 0.002) {
+    fragColor = vec4(255.0, 79.0, 0, 255.0)/255.0;
+    return;
+}"""
+
+@define
+class DocScene(DepthScene):
+    other: OnceTracker = Factory(OnceTracker)
+
+    static_plane: bool = False
+    focus_plane: bool = False
+
+    def build(self):
+        DepthScene.build(self)
+        self.shader.fragment = self.DEPTH_SHADER.read_text().replace(
+            "} else if (BACKWARD) {",
+            "} else if (BACKWARD) {" + SHADER_PATCH
+        )
+
+    def pipeline(self) -> Iterable[ShaderVariable]:
+        yield from DepthScene.pipeline(self)
+        yield ShaderVariable("uniform", "bool", "iStaticPlane", self.static_plane)
+        yield ShaderVariable("uniform", "bool", "iFocusPlane", self.focus_plane)
+
+    def _render_ui(self):
+        imgui.push_style_var(imgui.STYLE_WINDOW_BORDERSIZE, 2.0)
+        imgui.push_style_var(imgui.STYLE_WINDOW_ROUNDING, 8)
+        imgui.push_style_var(imgui.STYLE_GRAB_ROUNDING, 8)
+        imgui.push_style_var(imgui.STYLE_FRAME_ROUNDING, 8)
+        imgui.push_style_color(imgui.COLOR_FRAME_BACKGROUND, 0.1, 0.1, 0.1, 0.5)
+        imgui.new_frame()
+        imgui.set_next_window_position(0, 0)
+        imgui.set_next_window_bg_alpha(0.6)
+        imgui.begin("Parameters", False, imgui.WINDOW_ALWAYS_AUTO_RESIZE)
+        imgui.slider_float("Height",    self.state.height,    0, 1, "%.2f")
+        imgui.slider_float("Static",    self.state.static,    0, 1, "%.2f")
+        imgui.slider_float("Focus",     self.state.focus,     0, 1, "%.2f")
+        imgui.slider_float("Invert",    self.state.invert,    0, 1, "%.2f")
+        imgui.slider_float("Zoom",      self.state.zoom,      0, 2, "%.2f")
+        imgui.slider_float("Isometric", self.state.isometric, 0, 1, "%.2f")
+        imgui.slider_float("Dolly",     self.state.dolly,     0, 5, "%.2f")
+        imgui.slider_float("Offset X",  self.state.offset_x, -2, 2, "%.2f")
+        imgui.slider_float("Offset Y",  self.state.offset_y, -2, 2, "%.2f")
+        imgui.slider_float("Origin X",  self.state.origin_x, -2, 2, "%.2f")
+        imgui.slider_float("Origin Y",  self.state.origin_y, -2, 2, "%.2f")
+        imgui.slider_float("Center X",  self.state.center_x, -2, 2, "%.2f")
+        imgui.slider_float("Center Y",  self.state.center_y, -2, 2, "%.2f")
+        imgui.end()
+        imgui.pop_style_color()
+        imgui.pop_style_var(4)
+        imgui.render()
+
+        self._final.texture.fbo().use()
+        self.imgui.render(imgui.get_draw_data())
+
+        # Fixme: Dirty solution to frame zero issues
+        if not self.other():
+            self.next(dt=0)
+
+# ------------------------------------------------------------------------------------------------ #
+
+@define
+class DocsParameters:
+    estimator: DepthEstimator = Factory(DepthAnythingV2)
+    minio: BrokenMinio = Factory(BrokenMinio)
+
+    def render(self, scene: DepthScene, name: str, *, time: float=10):
+        # Find local and remote paths for the asset video
+        base = BROKEN.DIRECTORIES.SYSTEM_TEMP/"mkdocs"
+        file = Path(name).with_suffix(".mp4")
+        output = (base/file)
+
+        # Initialize and configure the scene
+        scene.estimator = self.estimator
+        scene.ffmpeg.h264(
+            preset="veryslow",
+            profile="high",
+            tune="film",
+            crf=28,
+            x264params=(
+                "ref=8",
+                "bframes=8",
+                "b-adapt=2",
+                "rc-lookahead=60",
+                "me=umh",
+                "subme=8",
+                "merange=24",
+                "analyse=all",
+                "trellis=2",
+                "deblock=-3,-3",
+                "psy-rd=1.0",
+                "aq-mode=2",
+                "aq-strength=1.0",
+            ),
+        )
+
+        # Render variations, upload
+        for _ in range(1):
+            video = scene.main(output=output, quality=100, ssaa=2, time=time)[0]
+            self.minio.upload(video, Path("depthflow")/output.relative_to(base))
+
+        # Cleanup
+        scene.window.destroy()
+
+    def make_height(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.height = (1 - math.cos(self.cycle))/2
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/height-varying.mp4"
+        )
+
+    def make_offset(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.offset_x = 1.5*math.sin(self.cycle)
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/offset-x-varying.mp4",
+            time=5
+        )
+
+        class Example(DocScene):
+            def update(self):
+                self.state.offset_x = math.cos(self.cycle)
+                self.state.offset_y = math.sin(self.cycle)
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/offset-xy-varying.mp4",
+            time=5
+        )
+
+    def make_static(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.offset_x = 1.5*math.sin(self.cycle)
+                self.state.static = 0.32
+
+        self.render(
+            scene=Example(static_plane=True),
+            name="learn/parameters/static-varying.mp4",
+            time=5
+        )
+
+    def make_isometric(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.height = 0.80
+                self.state.isometric = (1 - math.cos(self.cycle))/2
+                self.state.offset_x = 0.3*math.cos(self.cycle)
+                self.state.offset_y = 0.3*math.sin(self.cycle)
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/isometric-varying.mp4"
+        )
+
+        class Example(DocScene):
+            def update(self):
+                self.state.height = 0.80
+                self.state.isometric = 1.00
+                self.state.offset_x = 0.3*math.cos(self.cycle)
+                self.state.offset_y = 0.3*math.sin(self.cycle)
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/isometric-flat.mp4"
+        )
+
+    def make_dolly(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.height = 1
+                self.state.dolly = 2.5*(1 - math.cos(self.cycle))
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/dolly-varying.mp4"
+        )
+
+    def make_focus(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.height = 1.00
+                self.state.focus = 0.32
+                self.state.isometric = 0.999*(1 - math.cos(self.cycle))/2
+
+        self.render(
+            scene=Example(focus_plane=True),
+            name="learn/parameters/focus-varying.mp4",
+            time=5
+        )
+
+    def make_zoom(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.zoom = 1 + 0.5*(1 - math.cos(self.cycle))
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/zoom-varying.mp4"
+        )
+
+    def make_invert(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.height = 0.40
+                self.state.invert = (1 - math.cos(self.cycle))/2
+                self.state.offset_x = 0.5*math.sin(self.cycle)
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/invert-varying.mp4",
+        )
+
+    def make_center(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.center_x = math.sin(self.cycle)
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/center-varying.mp4",
+        )
+
+    def make_origin(self):
+        class Example(DocScene):
+            def update(self):
+                self.state.origin_x = 1
+                self.state.height = (1 - math.cos(self.cycle))/2
+
+        self.render(
+            scene=Example(),
+            name="learn/parameters/origin-varying.mp4",
+            time=5
+        )
+
+# ------------------------------------------------------------------------------------------------ #
+
+@define
+class DocsMath(manim.Scene):
+    ...
+
+# ------------------------------------------------------------------------------------------------ #
 
 def main():
-    ...
+    fabric = DocsParameters()
+    fabric.make_height()
+    fabric.make_offset()
+    fabric.make_static()
+    fabric.make_isometric()
+    fabric.make_dolly()
+    fabric.make_focus()
+    fabric.make_zoom()
+    fabric.make_invert()
+    fabric.make_center()
+    fabric.make_origin()
--- Website/learn/parameters.md
+++ Website/learn/parameters.md
@@ -2,58 +2,269 @@
 title: DepthFlow/Parameters
 ---
 
-All parameters are controlled within the [**state dictionary**](https://github.com/BrokenSource/DepthFlow/blob/main/DepthFlow/State.py) class
+<b><span class="the">T</span>his page</b> focus on all shader-related parameters, and how they affect the final image (in practical terms). The main goal is for aiding you to create your own animations. Nevertheless, it's useful to understand them for the animation system as well.
 
-## 🔴 Parallax parameters
+- For rendering and exporting parameters, see the [**ShaderFlow**](site:shaderflow) documentation.
 
-This section is about the **core parameters** of DepthFlow
+- For understanding the math, see the [**Foundations**](site:depthflow/learn/foundations) page!
 
 <hr>
+
+All parameters are controlled within the [**state dictionary**](https://github.com/BrokenSource/DepthFlow/blob/main/DepthFlow/State.py) class, acessible by:
+
+```python
+from DepthFlow import DepthScene
+
+scene = DepthScene()
+scene.state.height = 0.3
+scene.state.offset_x = 1
+```
+
+Or settable by the command line as a animation component:
+
+```python title="Terminal"
+# Note: This will only create a static image
+depthflow config --height 0.3 --offset_x 1 (...)
+```
+
+However, they are best used within the `.update()` method for creating animations:
+
+```python
+import math
+
+class YourScene(DepthScene):
+    def update(self):
+        self.state.offset_x = 0.3 * math.sin(self.cycle)
+```
+
+Internally, they are used and sent when the render method is called for the shader, which non-surprisingly happens in the main event loop of the Scene, after all `.update()`'s.
+
+Directly controlling those makes more sense when managing the code within Python (and bypassing the animation system, which essentially does the same thing) for writing custom animations and more advanced interactions or behaviors.
+
+
+## 🔴 Main parameters
+
+This section is about the **core parameters** of DepthFlow.
+
+- **Important**: When parameters refers to depth values, the value is _normalized_. A `static` value of $0.5$, with a `height` of $0.3$ means the perceptual `static` values is at $0.15$, (e.g.).
+
+- **Important**: Depth values of zero are the _farthest_ point, and values of one the _closest_.
+
+- **Note**: The suggested ranges aren't enforced, but what makes sense in most cases.
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
 
 ### Height
 
-<hr>
+> **Type:** `float`, **Range:** `[0, 1]`
 
-### Static
+<b><span class="the">T</span>he</b> `height` parameter defines the **peak height of the projected surface** at `depth=1`. It can be thought as the{== **effect's global intensity** ==}parameter.
 
-<hr>
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/height-varying.mp4" loop></video>
 
-### Focus
+!!! quote "It's arguably the **most important parameter**, virtually nothing happens without it"
 
-<hr>
+- A value of 0 means the surface is flat, while a value of 1 means the surface's peak is on the same $xy$ screen plane as the camera.
 
-### Zoom
+- Notice how in the video, the center doesn't touch the camera, as its `depth` value isn't $1$, but the closer bottom edge _"gets below"_ the camera's view.
 
-<hr>
+<!-- ------------------------------------------------------------------------------------------- -->
 
-### Isometric
-
-<hr>
-
-### Dolly
-
-<hr>
-
-### Invert
-
-<hr>
-
-### Mirror
-
-<hr>
+<hr class="thick-hr"/>
 
 ### Offset
 
+> **Type:** `Tuple[float, float]`, **Alias:** `offset_x`, `offset_y`, **Range:** `[-2, 2]`
+
+<b><span class="the">T</span>he</b> `offset` parameter defines the **parallax displacement** of the projected surface. It can be thought as the{== **camera's position** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/offset-x-varying.mp4" loop></video>
+
+!!! quote "This is the easiest way to add **'natural' movement** to the scene"
+
+- A value of 0 in a component means the surface and camera are centered, other values meaning depends on other parameters and the aspect ratio, _it's a bit experimental._
+
+- This parameter isn't a _"camera displacement"_ you might expect:
+    1. That would simply move the image around without changing the perspective, which is what the `center` parameter does.
+    2. The camera always _"looks"_ to the image (`origin` parameter) by adding an opposite bias to the ray's projection on how much the image is displaced.
+
 <hr>
+
+As you might expect, setting $x=cos(t)$ and $y=cos(t)$ parameter to follow a circular motion, will create a _"orbiting"_ effect around the center of the image.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/offset-xy-varying.mp4" loop></video>
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+### Static
+
+> **Type:** `float`, **Range:** `[-1, 1]`
+
+<b><span class="the">T</span>he</b> `static` parameter defines the **depth at which no offsets happen**. It can be thought as the{== **offsets focal depth** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/static-varying.mp4" loop></video>
+
+!!! quote "It's a great way of adding **subtle background movement** or **orbiting around a point**"
+
+- Notice how in the video, the orange line doesn't move when the `offset` changes, and the mirroring of relative directions when crossing this boundary.
+
+- This parameter makes the ray projections _"pivot"_ around this depth value internally.
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+### Isometric
+
+> **Type:** `float`, **Range:** `[0, 1]`
+
+<b><span class="the">T</span>he</b> `isometric` parameter defines **how much perspective is applied**. It can be thought as the{== **planification effect** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/isometric-varying.mp4" loop></video>
+
+!!! quote "It's the best way to **mitigate edge or stretching distortions**, at the cost of the 3D-ness of the video"
+
+- A value of 0 means full perspective projection, while a value of 1 means the image is projected as if it was isometric (all rays are parallel).
+    1. This completely negates the `height` parameter at `isometric=1`
+
+- This parameter makes effect more _"flat"_ and _"2D"_, in fact, a value of 1 turns offsets into a simple translation. A value of 0.5 is often recommended.
+
+<hr>
+
+Notice how in the video below the offsets are _"flattened"_, as if there was one layer per depth value and it was simply displaced in the $xy$ plane. Consequently, more of the image is visible, as the peak values don't race towards the camera as much, at the cost of being _flat_.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/isometric-flat.mp4" loop></video>
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+### Dolly
+
+> **Type:** `float`, **Range:** `[0, 10]`
+
+<b><span class="the">T</span>he</b> `dolly` parameter defines the **camera's distance from the image**. It's basically the same as the{== **isometric effect** ==}parameter, but with different units.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/dolly-varying.mp4"/>
+
+
+!!! quote "It's a great way for a **more natural isometric** effect control"
+
+- As you move away to objects, they appear more isometric, that's the reason why your face looks unnatural in close-up selfies.
+- A `dolly` value of 0 is the same as `isometric=0`
+- A `dolly` value of $\infty$ is the same as `isometric=1`
+
+As far as I know, the convertion factor between the two is given by:
+
+$$
+\text{isometric} = \left(\frac{2}{\pi}\right) \cdot \tan^{-1}\left(\text{dolly}\right)
+$$
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+### Focus
+
+> **Type:** `float`, **Range:** `[-1, 1]`
+
+<b><span class="the">T</span>he</b> `focus` parameter defines the **static depth on isometric changes**. It can be thought as the{== **isometric focal depth** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/focus-varying.mp4" loop></video>
+
+!!! quote "It's a great way to **add drama** to the scene, or **give attention** to an object"
+
+- Notice how in the video, the orange line doesn't move when the `isometric` changes, and the mirroring of perspective directions when crossing this boundary.
+
+- This parameter makes this depth value the $z=1$ plane internally.
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+### Zoom
+
+> **Type:** `float`, **Range:** `(0, 1]`
+
+<b><span class="the">T</span>he</b> `zoom` parameter defines the **camera's field of view**. It can be thought as the{== **you-know-it** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/zoom-varying.mp4" loop></video>
+
+!!! quote "It's a great way to **crop** the image"
+
+- A value of 1 means the image is fully visible, while a value of 2 means a quarter of the image is visible.
+
+- This is a _"digital zoom"_, it simply stretches the coordinates internally.
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+### Invert
+
+> **Type:** `float`, **Range:** `[0, 1]`
+
+<b><span class="the">T</span>he</b> `invert` parameter **interpolates between 0=far and 1=near and the opposite**. It can be thought as the{== **depth inversion** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/invert-varying.mp4" loop></video>
+
+!!! quote "This parameter is mostly useful when the input depth map is inverted"
+
+- A value of 0.5 flattens the depth map and nothing happens, while a value of 1 inverts the depth map. Middle values _can be thought as softening_ the depthmap.
+
+- It wraps the surface inside-out when the value is above 0.5, and a lot of encroaching will happen, as the background is now the foreground.
+
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
 
 ### Center
 
-<hr>
+> **Type:** `Tuple[float, float]`, **Range:** `([-ar, ar], [-1, 1])`
 
+<b><span class="the">T</span>he</b> `center` parameter defines the **center of the image**. It can be thought as the{== **raw offset** ==}parameter.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/center-varying.mp4" loop></video>
+
+!!! quote "This is the easiest way to **move the image around**"
+
+- A value of 0 in a component means the image is centered, other values applies a direct offset to the contents of the image.
+
+- This parameter is a _"camera displacement"_ you might expect, nothing fancy.
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
+
+        <!-- """Focal point of the offsets, *as if* the camera was above this point""" -->
 ### Origin
 
-<hr>
+> **Type:** `Tuple[float, float]`, **Range:** `([-ar, ar], [-1, 1])`
+
+<b><span class="the">T</span>he</b> `origin` parameter defines the **center point of offsets**. It can be thought {== **as if the camera was above this point** ==}, without moving it.
+
+<video src="https://assets.brokensrc.dev/depthflow/learn/parameters/origin-varying.mp4" loop></video>
+
+!!! quote "This is a good way to focus on a specific part of the image while feeling off-center"
+
+- The value sets _"the origin"_ of offsets to the projections of the image.
+
+- It is also the value at which height changes only causes zooming
+
+<!-- ------------------------------------------------------------------------------------------- -->
+
+<hr class="thick-hr"/>
 
 ## 🟡 Vignette parameters
 
+!!! warning "🚧 Work in Progress 🚧"
+
 ## 🟢 Depth of Field parameters
+
+!!! warning "🚧 Work in Progress 🚧"
+
