9d81cb4dea70d6b6bab8c1609f128b6f99b784d3
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-10-05 11:58:02 -0400

    Improve the batch exporting example powershell script

--- Examples/Batch.ps1
+++ Examples/Batch.ps1
@@ -1,23 +1,24 @@
 #!/usr/bin/env pwsh
 # (c) MIT License, Tremeschin
 #
-# This PowerShell script runs depthflow for all images on a "input" folder
-# in the current directory, and batch renders videos to "output"
-
-if (-not (Test-Path "output")) {
-    New-Item -ItemType Directory -Path "output"
-}
-
+# This PowerShell script runs depthflow for all images on a "inputs" folder
+# in the current directory, and batch renders videos to "outputs"
+#
+# Note: Save it as "run.ps1", right click and "Run with PowerShell" to execute
+#
+# Note: Grab a release and rename it to "depthflow.exe" on the same folder
+#
 Get-ChildItem -Path "inputs" | ForEach-Object {
     $filename = $_.BaseName
 
     # Change or add variations and parameters here!
-    depthflow.exe input -i $_.FullName `
-        main -o "./output/$filename.mp4"
+    .\depthflow.exe input -i $_.FullName `
+        orbital `
+        main -o "./outputs/$filename-orbital.mp4"
 
-    # depthflow.exe input -i $_.FullName `
-    #     dolly --intensity 0.5 `
-    #     main -o "./outputs/$filename-orbital.mp4"
+    .\depthflow.exe input -i $_.FullName `
+        horizontal --intensity 0.6 `
+        main -o "./outputs/$filename-horizontal.mp4" --loop 3
 }
 
 Write-Host "Press any key to continue..."
