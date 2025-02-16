5927dd898c3536645916120db27903bda25f481d
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-02-15 20:50:34 -0400

    Remove server file that needs restructuring and fell behind

--- DepthFlow/__main__.py
+++ DepthFlow/__main__.py
@@ -14,17 +14,11 @@ def gradio() -> None:
     from DepthFlow.Webui import DepthGradio
     BrokenTyper.simple(DepthGradio().launch)
 
-def server() -> None:
-    """🌐 Run DepthFlow's [bold green]API Server[/]"""
-    log.minor("Launching the DepthFlow API Server")
-    from DepthFlow.Server import DepthServer
-    DepthServer().cli(*sys.argv[1:])
-
 def main() -> None:
     with BrokenProfiler("DEPTHFLOW"):
         BrokenTyper.complex(
             main=depthflow,
-            nested=(depthflow, gradio, server),
+            nested=(depthflow, gradio),
             simple=BrokenTorch.install,
         )
 
