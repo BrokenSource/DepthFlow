a3cf16b98d2315a4b8615f4c7540eb826a8a1df4
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-03-03 21:14:33 -0400

    Simplify the build hook implementation searching for the main repo

--- .github/hatch_build.py
+++ .github/hatch_build.py
@@ -1,17 +1,12 @@
-import os
+import runpy
 from pathlib import Path
 
 from hatchling.metadata.plugin.interface import MetadataHookInterface
 
-path = Path.cwd()
-
-while (path != path.parent):
+for path in (cwd := Path.cwd(), *cwd.parents):
     if (hook := (path/"Broken"/"Hatch.py")).exists():
-        os.environ.setdefault("MONOREPO_ROOT", str(path))
-        exec(compile(hook.read_text("utf-8"), hook, "exec"), (scope := {}))
-        globals().update(scope)
+        globals().update(runpy.run_path(hook))
         break
-    path = path.parent
 else:
     class DummyHook(MetadataHookInterface):
         def update(self, metadata: dict) -> None:
